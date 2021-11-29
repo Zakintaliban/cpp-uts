862e433d277c0734147be37e21701dcd6d906800
Khasbullah Zakin <hasbizakin0804@gmail.com>
2021-11-29 14:03:18 +0700

    second commit

---
+++ soal2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ngulang;
+    int angka;
+
+    do{
+    cout << "silahkan pilih (1-3) jumlah uang yang akan anda ambil:";
+    cout << "\n1. 100.000";
+    cout << "\n2. 250.000";
+    cout << "\n3. 500.000\n";
+    cin >> angka;
+    switch (angka) {
+    case 1:
+        cout << "Silahkan ambil uang 100.000 anda";
+        break;
+    case 2:
+        cout << "Silahkan ambil uang 250.000 anda";
+        break;
+    case 3:
+        cout << "Silahkan ambil uang 500.000 anda";
+        break;
+    default:
+    cout << "masukan angka 1-3";
+    }
+    cout<<"\napakah ingin mengulang lagi?";
+    cin >> ngulang;
+    }
+    while (ngulang == 'y');
+    return 0;
+}
